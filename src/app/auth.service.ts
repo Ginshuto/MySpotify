253250f708e0db945383bee714fee0a90ca2361d
Matthieu MARTIN <ginshuto@MATTHIEU.localdomain>
2019-05-14 19:36:29 +0200

    4th commit

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   readonly client_ID: string = '12ebc58d644148119c27df63ef38fc7d';
-  readonly redirect_URI : string = "http://localhost:4200/login";
+  readonly redirect_URI : string = "https://ginshuto.github.io/MySpotify/login";
   readonly scopes : string = 'user-read-private user-read-email playlist-modify playlist-modify-public user-library-read playlist-modify-private';
   authorizeURL : string = "https://accounts.spotify.com/authorize?client_id="+
   this.client_ID + "&response_type=token&redirect_uri="+
